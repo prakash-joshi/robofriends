cb2e10c3ff9ac3095bfddaf811806531faa85ee1
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-12 23:00:19 +0530

    Created repetative Ui component Cards and added some data in robotData file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component, Fragment } from "react";
+import Card from "./Component/Card";
+import { robots } from "./Component/robotsData";
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className="f1 tc">
-          <h1>Hello World</h1>
-          <p>welcome to robofriend</p>
+        <div>
+          <Card id={robots[0].id} name={robots[0].name } email ={robots[0].email} />
+          <Card id={robots[1].id} name={robots[1].name } email ={robots[1].email} />
+          <Card id={robots[2].id} name={robots[2].name } email ={robots[2].email} />
         </div>
       </Fragment>
     );
