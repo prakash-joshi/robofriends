efa17ab09c545fbdbb98992ee1b83b0f08a508e1
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-12 23:31:08 +0530

    created cardlist and iterated the robos into cards componets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component, Fragment } from "react";
-import Card from "./Component/Card";
+import CardList from "./Component/CardList";
 import { robots } from "./Component/robotsData";
+
 
 export class App extends Component {
   render() {
     return (
       <Fragment>
         <div>
-          <Card id={robots[0].id} name={robots[0].name } email ={robots[0].email} />
-          <Card id={robots[1].id} name={robots[1].name } email ={robots[1].email} />
-          <Card id={robots[2].id} name={robots[2].name } email ={robots[2].email} />
+          <CardList robots={robots}/>
         </div>
       </Fragment>
     );
