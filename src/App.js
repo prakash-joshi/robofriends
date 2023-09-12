09c3f545a1121acff55f6abb282d4697e98e101a
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-09-13 00:48:34 +0530

    Added searchbar and filtered the robots with help of usestate to exchange data between sibling components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component, Fragment } from "react";
 import CardList from "./Component/CardList";
 import { robots } from "./Component/robotsData";
-
+import SearchBox from "./Component/SearchBox";
 
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value, });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.username.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <Fragment>
-        <div>
-          <CardList robots={robots}/>
+        <div className="tc">
+          <h1>Robo Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
         </div>
       </Fragment>
     );
